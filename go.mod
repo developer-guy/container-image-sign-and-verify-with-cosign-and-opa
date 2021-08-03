77adc265012ab542f2c7bde6d35779343f2c57a7
Batuhan Apaydın <batuhan.apaydin@trendyol.com>
2021-08-03 19:57:54 +0300

    initial commit
    Signed-off-by: Batuhan Apaydın <batuhan.apaydin@trendyol.com>

--- go.mod
+++ go.mod
@@ -3,14 +3,20 @@ module github.com/developer-guy/container-image-sign-and-verify-with-cosign-and-
 go 1.16
 
 require (
+	github.com/alvaroloes/enumer v1.1.2 // indirect
+	github.com/coreos/go-systemd v0.0.0-20190620071333-e64a0ec8b42a // indirect
 	github.com/docker/cli v20.10.0-beta1.0.20201117192004-5cc239616494+incompatible // indirect
 	github.com/docker/docker v20.10.0-beta1.0.20201110211921-af34b94a78a1+incompatible // indirect
+	github.com/golangci/golangci-lint v1.17.2-0.20190910081718-bad04bb7378f // indirect
 	github.com/google/go-containerregistry v0.5.1
+	github.com/google/monologue v0.0.0-20190606152607-4b11a32b5934 // indirect
+	github.com/google/trillian-examples v0.0.0-20190603134952-4e75ba15216c // indirect
 	github.com/julienschmidt/httprouter v1.3.0
-	github.com/sigstore/cosign v0.6.1-0.20210713005353-82d49dcf3b8b
-	github.com/sigstore/rekor v0.2.1-0.20210712122031-1c30d2ff9518 // indirect
+	github.com/letsencrypt/pkcs11key v2.0.1-0.20170608213348-396559074696+incompatible // indirect
+	github.com/mattn/go-sqlite3 v1.10.0 // indirect
+	github.com/sigstore/cosign v1.0.1
+	go.etcd.io/etcd v3.3.13+incompatible // indirect
 	golang.org/x/time v0.0.0-20210611083556-38a9dc6acbc6 // indirect
-	gopkg.in/check.v1 v1.0.0-20200902074654-038fdea0a05b // indirect
 	gotest.tools/v3 v3.0.3 // indirect
 )
 
--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 package main
 
-import 	(
+import (
 	"context"
 	"encoding/json"
 	"fmt"
@@ -11,21 +11,22 @@ import 	(
 
 	"github.com/google/go-containerregistry/pkg/name"
 	"github.com/julienschmidt/httprouter"
+	"github.com/sigstore/cosign/cmd/cosign/cli"
+	"github.com/sigstore/cosign/cmd/cosign/cli/fulcio"
 	"github.com/sigstore/cosign/pkg/cosign"
-	"github.com/sigstore/cosign/pkg/cosign/fulcio"
 )
 
-type ImageVerificationReq struct {
+type VerificationReq struct {
 	Image string
 }
 
-type ImageVerificationResp struct {
+type VerificationResp struct {
 	Verified            bool   `json:"verified"`
 	VerificationMessage string `json:"verification_message"`
 }
 
-func Verify(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	var body ImageVerificationReq
+func VerifySig(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	var body VerificationReq
 	err := json.NewDecoder(r.Body).Decode(&body)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
@@ -53,19 +54,73 @@ func Verify(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	}
 
 	co := &cosign.CheckOpts{
-		RootCerts:  fulcio.Roots,
+		RootCerts:   fulcio.Roots,
 		SigVerifier: key,
-		Claims: true,
+		Claims:      true,
 	}
 
-	var resp ImageVerificationResp
+	var resp VerificationResp
 	if _, err = cosign.Verify(ctx, ref, co); err != nil {
-		resp = ImageVerificationResp{
+		resp = VerificationResp{
 			Verified:            false,
 			VerificationMessage: err.Error(),
 		}
 	} else {
-		resp = ImageVerificationResp{
+		resp = VerificationResp{
+			Verified:            true,
+			VerificationMessage: fmt.Sprintf("valid signatures found for an image: %s", body.Image),
+		}
+	}
+
+	respAsByte, err := json.Marshal(resp)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(respAsByte)
+}
+func VerifyAttest(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	var body VerificationReq
+	err := json.NewDecoder(r.Body).Decode(&body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	ctx := context.TODO()
+
+	wDir, err := os.Getwd()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	verifier, err := cli.LoadPublicKey(ctx, filepath.Join(wDir, "cosign.pub"))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	ref, err := name.ParseReference(body.Image)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	co := &cosign.CheckOpts{
+		RootCerts:            fulcio.Roots,
+		RegistryClientOpts:   cli.DefaultRegistryClientOpts(ctx),
+	}
+	co.SigVerifier = verifier
+	var resp VerificationResp
+	if _, err = cosign.Verify(ctx, ref, co); err != nil {
+		resp = VerificationResp{
+			Verified:            false,
+			VerificationMessage: err.Error(),
+		}
+	} else {
+		resp = VerificationResp{
 			Verified:            true,
 			VerificationMessage: fmt.Sprintf("valid signatures found for an image: %s", body.Image),
 		}
@@ -82,6 +137,7 @@ func Verify(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 
 func main() {
 	router := httprouter.New()
-	router.POST("/verify", Verify)
+	router.POST("/verify-signature", VerifySig)
+	router.POST("/verify-attestation", VerifyAttest)
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
